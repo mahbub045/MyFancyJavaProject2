f178cc4290a90abab9f45b34b9a78490c7ae34d6
mahbub099 <mahbubrmn99@gmail.com>
2022-01-06 00:26:47 +0600

    Added first sample code.

---
+++ src/com/company/sampleProject.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class sampleProject {
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+        System.out.println("This is our Assignment.");
+
+    }
+}
