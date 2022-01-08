d7d5f8ead31dd5c73931925131f5b03e79ac7ce6
mnaeem182060@bscse.uiu.ac.bd <naeem979>
2022-01-08 22:00:29 +0600

    After implementing Singleton Pattern

--- src/com/company/sampleProject.java
+++ src/com/company/sampleProject.java
@@ -1,9 +1,47 @@
 package com.company;
 
+
+class DbConnection{
+
+    private static DbConnection instance=null;
+
+    private DbConnection(String dbUser,String dbPassword,String dbName ) {
+        SQLConnection connection = new SQLConnection(dbUser, dbPassword, dbName);
+    }
+
+
+    public static DbConnection getDbConnection(String dbUser,String dbPassword,String dbName) {
+        if (instance== null )
+            instance = new DbConnection(dbUser,dbPassword,dbName) ;
+
+        return instance;
+    }
+}
+class SQLConnection {
+
+    String dbUser;
+    String dbPassword;
+    String dbName;
+
+    public SQLConnection(String dbUser, String dbPassword, String dbName) {
+        super();
+        this.dbUser = dbUser;
+        this.dbPassword = dbPassword;
+        this.dbName = dbName;
+    }
+
+
+}
+
 public class sampleProject {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         System.out.println("Hello world.");
         System.out.println("This is our Assignment.");
+
+        String dbUser="admin";
+        String dbPassword="admin";
+        String dbName="tesDb";
+        DbConnection connection=DbConnection.getDbConnection(dbUser, dbPassword, dbName);
 
     }
 }
